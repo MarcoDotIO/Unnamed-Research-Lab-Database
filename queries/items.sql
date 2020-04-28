f7dae088d316f832f9acef9622193dfed5db3465
Devin Kyser <github@clickdevin.me>
2020-04-28 10:31:35 -0400

    Item checkouts

--- queries/items.sql
+++ queries/items.sql
@@ -2,4 +2,16 @@
 select * from items_ext where item_id = :item_id;
 
 -- :name find_items :many
-select * from items_ext where item_name like :item_name
+select * from items_ext where item_name like :item_name;
+
+-- :name get_items_by_loanee :many
+select * from items_ext where user_id = :user_id;
+
+-- :name return_item :affected
+update checkouts set active = false
+    where item_id = :item_id and active = 1;
+
+-- :name checkout :insert
+insert into checkouts
+    (item_id, user_id, checkout_time, due_time, active) values
+    (:item_id, :user_id, curtime(), curtime(), 1);
--- research_lab/routes/items.py
+++ research_lab/routes/items.py
@@ -1,11 +1,26 @@
 from research_lab import app, db, login_required
-from flask import render_template, request
+from flask import render_template, request, redirect
+from flask_login import current_user
 
 @app.route('/items/')
 @login_required
 def items():
     q = request.args.get('q', default='')
-    if q == '':
-        return render_template('items.j2', items=db.find_items(item_name='%'))
-    else:
-        return render_template('items.j2', items=db.find_items(item_name='%' + q + '%'))
+    return render_template('items.j2',
+        checkouts=db.get_items_by_loanee(user_id=current_user.details['user_id']),
+        items=db.find_items(item_name='%' + q + '%')
+    )
+
+@app.route('/items/<int:item_id>/checkout/')
+@login_required
+def checkout_item(item_id):
+    if db.get_item_by_id(item_id=item_id)['checkout_id'] == None:
+        db.checkout(user_id=current_user.details['user_id'], item_id=item_id)
+    return redirect('/items/')
+
+@app.route('/items/<int:item_id>/return/')
+@login_required
+def return_item(item_id):
+    if db.get_item_by_id(item_id=item_id)['user_id'] == current_user.details['user_id']:
+        db.return_item(item_id=item_id)
+    return redirect('/items/')
